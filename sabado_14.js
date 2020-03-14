d2ffa4346130c90b051ad5bfdae01999e184987a
Carolina Picado <49088100+Caro10@users.noreply.github.com>
2020-03-14 16:21:32 -0600

    Add files via upload

---
+++ sabado_14.js
@@ -0,0 +1,10 @@
+var  dibujarDiagonal = function(numeroAsteriscos){
+    let asterisco = "*"
+    for(index = 0;index< numeroAsteriscos;index++){
+        console.log(asterisco)
+        asterisco = " " + asterisco
+    }
+}
+
+
+
